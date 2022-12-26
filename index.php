89a18502280809674c129ac25c9408c39dc6fdbd
kraigochieng <65193721+kraigochieng@users.noreply.github.com>
2022-12-26 17:47:28 +0300

    Project Done

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php
+            include('components/meta.php');
+            include('components/link.php');
+        ?>
+    </head>
+    <body>
+        <div id="qr-code-container">
+            <img id="qr-code-image" src="images/image-qr-code.png"/>
+            <p id="qr-code-title">Improve your front-end skills by building projects</p>
+            <p id="qr-code-description">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+    </body>
+</html>
